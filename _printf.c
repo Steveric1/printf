8d44e9a695f74af95790e526363eebc70b05749a
Eric <stephen.erich243@gmail.com>
2023-07-26 16:22:19 -0700

    group project printf

--- _printf.c
+++ _printf.c
@@ -21,7 +21,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
